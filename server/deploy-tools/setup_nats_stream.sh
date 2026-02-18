95608f90ae3a951f3a32b071da487e44dada86e3
David Pitkin <dpitkin@vividsolutions.com>
2026-02-17 16:23:29 -0800

    PIM-2454: Adding GitHub Actions for deploying NATS.

--- server/deploy-tools/setup_nats_stream.sh
+++ server/deploy-tools/setup_nats_stream.sh
@@ -4,10 +4,9 @@
 # Intended to be run as pit_admin.
 
 # Create stream.
-nats stream add --subjects="policies-event-channel" --storage=file --replicas=3 --retention=interest --discard=new --defaults policies-event-channel
+nats stream add --subjects="policies-event-channel" --storage=file --replicas=#{MSG_QUEUE_REPLICAS}# --retention=interest --discard=new --defaults policies-event-channel
 
 # Create Consumers.
 nats consumer add --ack=explicit --deliver=all --pull --backoff=linear --backoff-steps=20 --backoff-min=5s --backoff-max=60m --max-deliver=150 --max-pending=1000 --defaults policies-event-channel claims_sync_rest_consumer
 
 nats consumer add --ack=explicit --deliver=all --pull --backoff=linear --backoff-steps=20 --backoff-min=5s --backoff-max=60m --max-deliver=150 --max-pending=1000 --defaults policies-event-channel underwriting_sync_rest_consumer
-
